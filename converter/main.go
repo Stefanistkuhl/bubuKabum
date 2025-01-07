6ead9c3ad1253095b84f670d5a164cdb11459fbe
Stefanistkuhl <ghgstefan@gmail.com>
2025-01-07 20:29:28 +0100

    Removed uncessarry condition in parse_data

--- converter/main.go
+++ converter/main.go
@@ -36,7 +36,7 @@ func get_emote(emote_url string) {
 	data := make_request(emote_url)
 	emote := parse_data(data)
 	fmt.Println(emote)
-	download_emote(emote.id, emote.emote_name, emote.metadata.filename)
+	download_emote(emote)
 }
 
 func parse_data(data []byte) *Emote {
@@ -47,29 +47,15 @@ func parse_data(data []byte) *Emote {
 	file := gjson.GetBytes(data, "host.files.11")
 	file_extension := file.Get("name").String()
 	state := gjson.GetBytes(data, "animated").Bool()
-	if state == true {
-		size := file.Get("size").Int()
-		frame_count := file.Get("frame_count").Int()
-		emote.emote_name = emote_name
-		emote.animated = state
-		emote.id = id
-		emote.metadata.size = size
-		emote.metadata.filename = file_extension
-		emote.metadata.frame_count = frame_count
-		return emote
-
-	} else {
-		size := file.Get("size").Int()
-		frame_count := file.Get("frame_count").Int()
-		fmt.Println("this emote is not animated")
-		emote.emote_name = emote_name
-		emote.animated = state
-		emote.id = id
-		emote.metadata.size = size
-		emote.metadata.filename = file_extension
-		emote.metadata.frame_count = frame_count
-		return emote
-	}
+	size := file.Get("size").Int()
+	frame_count := file.Get("frame_count").Int()
+	emote.emote_name = emote_name
+	emote.animated = state
+	emote.id = id
+	emote.metadata.size = size
+	emote.metadata.filename = file_extension
+	emote.metadata.frame_count = frame_count
+	return emote
 }
 
 func make_request(url string) []byte {
@@ -90,9 +76,9 @@ func make_request(url string) []byte {
 	return data
 }
 
-func download_emote(emote_id string, emote_name string, file_extension string) {
-	fileName := emote_name + file_extension
-	emote_url := CDN_URL + emote_id + "/" + file_extension
+func download_emote(emote *Emote) {
+	fileName := emote.emote_name + emote.metadata.filename
+	emote_url := CDN_URL + emote.id + "/" + emote.metadata.filename
 	// Create blank file
 	file, err := os.Create(fileName)
 	if err != nil {
